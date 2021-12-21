5659cfb3b43ead270e52d8a88e2cd0911fe0f547
bryahchin.mihail <bryahchin.mihail@otr.ru>
2021-12-21 15:30:32 +0300

    Initial commit

---
+++ src/main/java/otus/integration/exp/OtusIntegrationExpApplication.java
@@ -0,0 +1,13 @@
+package otus.integration.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusIntegrationExpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusIntegrationExpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/otus/integration/exp/OtusIntegrationExpApplicationTests.java
@@ -0,0 +1,13 @@
+package otus.integration.exp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusIntegrationExpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
